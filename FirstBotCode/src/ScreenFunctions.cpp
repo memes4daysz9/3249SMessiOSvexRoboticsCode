c66461788a6667cc16989625059c43ec097253c4
memes4daysz9 <73305439+memes4daysz9@users.noreply.github.com>
2024-01-24 12:13:07 -0800

    added on-screen messages

--- FirstBotCode/src/ScreenFunctions.cpp
+++ FirstBotCode/src/ScreenFunctions.cpp
@@ -8,7 +8,6 @@ int AutonSide;
 
 void ScreenStats(){
     pros::screen_touch_status_s_t TouchPos;
-    pros::lcd::set_text(1, "Select Autonomous"); // screen is 480 by  240 pixels, meaning that half is at 240 by 100
     pros::screen::set_pen(COLOR_RED);
     pros::screen::fill_rect(1,100,480,200);
     pros::screen::set_pen(COLOR_BLUE);
@@ -16,6 +15,13 @@ void ScreenStats(){
     pros::screen::set_pen(COLOR_BLACK);
     pros::screen::draw_line(240,0,240,200); //Vertical Line
     pros::screen::draw_line(0,100,480,100); //Horizontal Line
+    pros::screen::print(pros::TEXT_SMALL,60,25,"BlueSideNoMatchLoading");
+    pros::screen::print(pros::TEXT_SMALL,120,25,"BlueSideYesMatchLoading");
+    pros::screen::print(pros::TEXT_SMALL,60,75,"RedSideYesMatchLoading");
+    pros::screen::print(pros::TEXT_SMALL,120,75,"RedSideNoMatchLoading");
+    pros::screen::set_pen(COLOR_WHITE);
+    pros::screen::print(pros::TEXT_SMALL,240,100, "Select Autonomous"); // screen is 480 by  240 pixels, meaning that half is at 240 by 100
+
     while (true){
         if ((autonSelected = false)){
             if (TouchPos.y > 100){//blue Side
