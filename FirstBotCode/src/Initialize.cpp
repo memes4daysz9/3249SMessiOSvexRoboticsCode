64e18672b3d1e054a5f2e8d145d261eaae042992
memes4daysz9 <73305439+memes4daysz9@users.noreply.github.com>
2024-02-12 11:36:10 -0800

    pnumatics fix

--- FirstBotCode/src/Initialize.cpp
+++ FirstBotCode/src/Initialize.cpp
@@ -3,6 +3,10 @@ void initialize() {
 
     pros::Task OdomTask(OdomTracking); //multithreading W
     pros::Task ScreenTask(ScreenStats);
+    pros::ADIDigitalOut FirstWingMan(1 ,'a');
+	pros::ADIDigitalOut SecondWingMan(2 ,'b');
+    FirstWingMan.set_value(LOW);//da kenny fix
+    SecondWingMan.set_value(LOW);
     //MotorsInitialization
     pros::Motor FrontLeftMotor_initializer(1, pros::E_MOTOR_GEARSET_18, false,pros::E_MOTOR_ENCODER_COUNTS);
     pros::Motor FrontRightMotor_initializer(2,pros::E_MOTOR_GEARSET_18, true,pros:: E_MOTOR_ENCODER_COUNTS);
@@ -13,6 +17,7 @@ void initialize() {
     pros::Motor Intake_initializer(6, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
     pros::Motor LeftShotBlock_initializer(7, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
     pros::Motor RightShotBlock_initializer(8, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
+
     if (pros::usd::is_installed()){
         ofstream CurrentLog ("/usd/LogData.txt");
     }
