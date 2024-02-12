c8ba72b99def26724d77a9c080140a5563080073
memes4daysz9 <73305439+memes4daysz9@users.noreply.github.com>
2024-02-12 07:50:30 -0800

    AutonFix0.6

--- FirstBotCode/include/FileSys.h
+++ FirstBotCode/include/FileSys.h
@@ -2,7 +2,11 @@
 
 #define PIDMaxLines 7 // Maximum number of lines to read
 
-extern void WriteSaveConfig(std::string FileSave);
-extern std::string ReadSaveConfig(std::string FilePath, int LineNumber);
-
-extern char line[PIDMaxLines][100]; // PID Array to store lines
+class SysFiles{
+    public:
+    char line[PIDMaxLines][100]; // PID Array to store lines
+    SysFiles();
+    void WriteSaveConfig(string FileSave);
+    string ReadSaveConfig(string FilePath, int LineNumber);    
+}
+extern SysFiles SysFiles;
--- FirstBotCode/src/Auton.cpp
+++ FirstBotCode/src/Auton.cpp
@@ -9,63 +9,63 @@ void autonomous(){
 	pros::Motor CataMotor(5);
 
 if(AutonSide == 1){
-Forward(5);
-Rotate(90);
-Forward(6);
-Rotate(-45);
+Odom::Forward(5);
+Odom::Rotate(90);
+Odom::Forward(6);
+Odom::Rotate(-45);
 SecondWingMan.set_value(HIGH);
-Forward(6);
-Rotate(-45);
+Odom::Forward(6);
+Odom::Rotate(-45);
 SecondWingMan.set_value(LOW);
-Forward(24);
+Odom::Forward(24);
 //prime flywheel
-RunFlywheel(1200);
+Odom::RunFlywheel(1200);
 pros::delay(3000);
-RunFlywheel(0);
+Odom::RunFlywheel(0);
 
 }else if (AutonSide == 2){
 //yes matchload blue
- Forward(5);
- Rotate(-135);
+ Odom::Forward(5);
+ Odom::Rotate(-135);
  FirstWingMan.set_value(HIGH);
- Forward(7);
- Rotate(225);
- Forward(6);
- Rotate(90);
+ Odom::Forward(7);
+ Odom::Rotate(225);
+ Odom::Forward(6);
+ Odom::Rotate(90);
  SecondWingMan.set_value(HIGH);
- Forward(36);
+ Odom::Forward(36);
 
  FirstWingMan.set_value(LOW);
  SecondWingMan.set_value(LOW);
- Forward(-24); //return to matchloader
- Rotate(-45);
- Forward(-24);
+ Odom::Forward(-24); //return to matchloader
+ Odom::Rotate(-45);
+ Odom::Forward(-24);
  //prime flywheel
-RunFlywheel(1200);
+Odom::RunFlywheel(1200);
 pros::delay(3000);
-RunFlywheel(0);
+Odom::RunFlywheel(0);
 
 }else if(AutonSide == 3){
 //Red Yes matchload
-Forward(5);
-Rotate(-45);
+Odom::Forward(5);
+Odom::Rotate(-45);
 FirstWingMan.set_value(HIGH);
-Forward(12);
-Rotate(135);
-Forward(36);
-Rotate(-45);
+Odom::Forward(12);
+Odom::Rotate(135);
+Odom::Forward(36);
+Odom::Rotate(-45);
 SecondWingMan.set_value(HIGH);
-Forward(36);
+Odom::Forward(36);
 SecondWingMan.set_value(LOW);
 FirstWingMan.set_value(LOW);
 //return to matchloader
-Forward(-36);
-Rotate(-45);
-Forward(-36);
+Odom::Forward(-36);
+Odom::Rotate(-45);
+Odom::Forward(-36);
 //prime flywheel
-RunFlywheel(1200);
+Odom::RunFlywheel(1200);
 pros::delay(3000);
-RunFlywheel(0);
+Odom::RunFlywheel(0);
 
 }else if (AutonSide == 4){
 
